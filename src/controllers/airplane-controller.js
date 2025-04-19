c04671e152deeca96662f73b118f62e6a018a736
siddharthpalav <siddharthapalav@gmail.com>
2025-04-19 14:27:18 +0530

    Added get all airplanes api

---
+++ src/controllers/airplane-controller.js
@@ -0,0 +1,39 @@
+const { StatusCodes } = require('http-status-codes');
+const { AirplaneService } = require('../services');
+const { SuccessReponse, ErrorResponse } = require('../utils/common');
+
+/**
+ *
+ * POST : /airplanes
+ * req-body {modelNumber: 'airbus320', capacity: 200}
+ */
+async function createAirplane(req, res) {
+	try {
+		const airplane = await AirplaneService.createAirplane({
+			modelNumber: req.body.modelNumber,
+			capacity: req.body.capacity
+		});
+		SuccessReponse.message = 'Successfully created an airplane';
+		SuccessReponse.data = airplane;
+		return res.status(StatusCodes.CREATED).json(SuccessReponse);
+	} catch (error) {
+		ErrorResponse.error = error;
+		return res.status(error.statusCode).json(ErrorResponse);
+	}
+}
+
+async function getAirplanes(req, res) {
+	try {
+		const airplanes = await AirplaneService.getAirplanes();
+		SuccessReponse.data = airplanes;
+		return res.status(StatusCodes.OK).json(SuccessReponse);
+	} catch (error) {
+		ErrorResponse.error = error;
+		return res.status(error.statusCode).json(ErrorResponse);
+	}
+}
+
+module.exports = {
+	createAirplane,
+	getAirplanes
+};
---
+++ src/utils/common/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+	SuccessReponse: require('./success-response'),
+	ErrorResponse: require('./error-response')
+};
