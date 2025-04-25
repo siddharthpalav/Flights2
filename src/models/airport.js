22baafef4d291982ef35beb79e083b58c2444c40
siddharthpalav <siddharthapalav@gmail.com>
2025-04-25 15:16:03 +0530

    Added new migration to add foreign key constraint in one to many relation

--- src/models/airport.js
+++ src/models/airport.js
@@ -11,8 +11,7 @@ module.exports = (sequelize, DataTypes) => {
 			// define association here
 			this.belongsTo(models.City, {
 				foreignKey: 'cityId',
-				onDelete: 'CASCADE',
-				onUpdate: 'CASCADE'
+				onDelete: 'CASCADE'
 			});
 		}
 	}
